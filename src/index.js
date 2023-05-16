6e99bcf74113174342d6f52bda52f7627720a6d3
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2023-05-16 12:01:35 -0700

    fix errors

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='process.env.PUBLIC_URL'>
+  <React.Fragment>
     <App />
-  </BrowserRouter>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
